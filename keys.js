edfdfdb0be9042097736ddcb3c91eb4715e1ef89
Lyn Vierra <lynvierra@gmail.com>
2018-07-28 18:40:55 -0700

    push 2

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: process.env.0DPeyshIUzgCl0uSbLWoKJ9ti,
+  consumer_secret: process.env.bQs9BackcHWf0cR34gWBTqlE2iMHb5FuZpj3Fv9fIv5ExHVKCL,
+  access_token_key: process.env.1021107004331384832-xZB9xMZc9icehc6EAnQtLnrLmtSsBG,
+  access_token_secret: process.env.eqYpVOH1n5DCPZ8jojNxBiY4wHgE3Yr5rh2GOFFQnHKI9,
+
+};
+
+exports.spotifyKeys = {
+  id: process.env.03f4ff3d6c7d4d379ac5acacc0cfdb5f,
+  secret: process.env.e508761cd5d0406096789cb22bda4939,
+};
